c41a71352be07c3c6b487baf3749db3a9fff6742
Arundev <devarun07@gmail.com>
2022-05-09 20:20:07 +0300

    first test changes

--- Communication/Host_node.py
+++ Communication/Host_node.py
@@ -1,11 +1,14 @@
 import socket
 import threading
 import pickle
+import time
+import random
+
 import numpy as np
 
 
 # Replace it with the local IP of Host machine
-host = "127.0.0.1"
+host = "10.100.26.70"
 port = 5050
 
 
@@ -19,9 +22,9 @@ client, address = server.accept()
 
 
 # Create global variables
-motor_cmd = np.zeros(8)
+motor_cmd = [120, 110, 100, 90, 80, 70, 60, 50]
 # 8 motor states, 3 acceleration, 2 velocity
-robot_states = np.zeros(13)
+robot_states = [0]*13
 
 lock = threading.Lock()
 
@@ -30,29 +33,54 @@ lock = threading.Lock()
 def receive():
     connected = True
     while connected:
-        message = client.recv(1024)
-        robot_states_nw = pickle.loads(message)
-        with lock:
-            global robot_states
-            robot_states = robot_states_nw
+        try:
+            message = client.recv(1024)
+            robot_states_nw = pickle.loads(message)
+            with lock:
+                global robot_states
+                robot_states = robot_states_nw
+                print(robot_states)
+            time.sleep(0.2)
+
+        except Exception as e:
+            print("Exception in receive: {}".format(e))
+            break
 
 
 # Send values to Raspberry Pi
 def send():
-    with lock:
-        client.send(motor_cmd)
+    connected = True
+    while connected:
+        try:
+            with lock:
+                # global motor_cmd
+                # motor_cmd = [val-2 for val in motor_cmd]
+                motor_cmd_send = pickle.dumps(motor_cmd)
+                client.send(motor_cmd_send)
+            time.sleep(0.2)
+
+        except Exception as e:
+            print("Exception in send: {}".format(e))
+            break
 
 
 def run_RL_algorithm():
     # This function uses robot_states and writes into motor_cmd
-    return
+    while True:
+        with lock:
+            global motor_cmd
+            motor_cmd = [x-2 for x in motor_cmd]
+        time.sleep(2)
 
 
 # Create two threads to run send and receive in parallel
 receive_thread = threading.Thread(target=receive, args=())
 send_thread = threading.Thread(target=send, args=())
 
-receive_thread.start()
-send_thread.start()
 
+send_thread.start()
+#send_thread.join()
+time.sleep(1)
+
+receive_thread.start()
 run_RL_algorithm()
--- Communication/Teesny/Teesny.ino
+++ Communication/Teesny/Teesny.ino
@@ -1,9 +1,11 @@
+#include <Arduino.h>
+#include <i2c_driver.h>
 #include <i2c_driver_Wire.h>
 #include <Servo.h>
 #include <sstream>
 using namespace std;
 
-
+int count = 0;
 Servo servo1;
 Servo servo2;
 Servo servo3;
@@ -18,60 +20,82 @@ void receiveEvent(int howMany)
   Serial.println("Inside Receive");
 
   // Ignore first value as it is the address of the register
-  Wire1.read()
-  
+  Wire1.read();
+
   byte rxbyte;
-  int motor_cmd[8];
-  idx = 0
-  
-  # Read next 8 values
+  int motor_cmd[9];
+  int idx = 0;
+  int true_request = 0;
+
+  // Read next 8 values
   while (Wire1.available()) {
     rxbyte = Wire1.read();
     Serial.println("Data received:");
     Serial.println(rxbyte);
-    motor_cmd[idx] = rxbyte;
+    motor_cmd[idx] = int(rxbyte);
+    idx = idx + 1;
+    if (idx == 7) {
+      true_request = 1;
+    }
   }
 
-  for (int idx; idx < 8; idx++) {
-    switch (idx) {
-      case 0:
-      Servo1.write(motor_cmd[idx]);
+  if (true_request == 1) {
+    Serial.println("Size of array:");
+    Serial.println(sizeof(motor_cmd));
+    for (int i = 0; i < 8; i++) {
+      Serial.println(motor_cmd[i]);
+    }
+  }
 
-      case 1:
-      Servo2.write(motor_cmd[idx]);
 
-      case 2:
-      Servo3.write(motor_cmd[idx]);
 
-      case 3:
-      Servo4.write(motor_cmd[idx]);
+  if (true_request == 1) {
+    for (int idx = 0; idx < 8; idx++) {
+      Serial.println("To switch");
+      switch (idx) {
+        case 0:
+          servo1.write(motor_cmd[idx]);
 
-      case 4:
-      Servo5.write(motor_cmd[idx]);
+        case 1:
+          servo2.write(motor_cmd[idx]);
 
-      case 5:
-      Servo6.write(motor_cmd[idx]);
+        case 2:
+          servo3.write(motor_cmd[idx]);
 
-      case 6:
-      Servo7.write(motor_cmd[idx]);
+        case 3:
+          servo4.write(motor_cmd[idx]);
 
-      case 7:
-      Servo8.write(motor_cmd[idx]);
+        case 4:
+          servo5.write(motor_cmd[idx]);
+
+        case 5:
+          servo6.write(motor_cmd[idx]);
+
+        case 6:
+          servo7.write(motor_cmd[idx]);
+
+        case 7:
+          servo8.write(motor_cmd[idx]);
+      }
     }
   }
 }
 
 void requestEvent() {
- Serial.println("Inside Request");
- Wire1.write(Servo1.read()); 
- Wire1.write(Servo2.read());
- Wire1.write(Servo3.read());
- Wire1.write(Servo4.read());
- Wire1.write(Servo5.read());
- Wire1.write(Servo6.read());
- Wire1.write(Servo7.read());
- Wire1.write(Servo8.read());
- }
+  Serial.println("Inside Request");
+  Serial.println(servo1.read());
+  Serial.println(servo2.read());
+  Serial.println(servo3.read());
+  Serial.println("----------\n");
+  Wire1.write(servo1.read());
+  Wire1.write(servo2.read());
+  Wire1.write(servo3.read());
+  Wire1.write(servo4.read());
+  Wire1.write(servo5.read());
+  Wire1.write(servo6.read());
+  Wire1.write(servo7.read());
+  Wire1.write(servo8.read());
+}
 
 void setup() {
   // put your setup code here, to run once:
